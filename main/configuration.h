17c474bf61b03c95cfc922710d71af97ae850921
Björn Schrader <mail@bjoern-schrader.de>
2021-11-08 09:01:33 +0100

    Change SF to 7
    ttn network map uses SF7 as default setting for mapping.

--- main/configuration.h
+++ main/configuration.h
@@ -69,7 +69,7 @@ void ttn_register(void (*callback)(uint8_t message));
 #define LOGO_DELAY              5000            // Time to show logo on first boot
 #define LORAWAN_PORT            10              // Port the messages will be sent to
 #define LORAWAN_CONFIRMED_EVERY 0               // Send confirmed message every these many messages (0 means never)
-#define LORAWAN_SF              DR_SF10         // Spreading factor (recommended DR_SF7 for ttn network map purposes, DR_SF10 works for slow moving trackers)
+#define LORAWAN_SF              DR_SF7          // Spreading factor (recommended DR_SF7 for ttn network map purposes, DR_SF10 works for slow moving trackers)
 #define LORAWAN_ADR             0               // Enable ADR
 #define REQUIRE_RADIO           true            // If true, we will fail to start if the radio is not found
 
